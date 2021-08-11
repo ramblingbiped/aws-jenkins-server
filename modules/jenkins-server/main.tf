6502c230f9ffb88a6b97ec0aae52661e45df7e07
Will Nave <willnave@Wills-MacBook-Pro.local>
2021-08-11 15:45:28 -0500

    Remove ingress and egress rules from the security group resource and add them as individual rule resources per best practices

--- modules/jenkins-server/main.tf
+++ modules/jenkins-server/main.tf
@@ -38,46 +38,35 @@ resource "aws_security_group" "jenkins-server-sg" {
     description = "${var.deployment_name} ec2 instance security group"
     vpc_id = module.vpc.vpc_id
 
-    ingress = [
-        {
-            description = "Allow HTTP from Public"
-            from_port = var.jenkins_port
-            to_port = var.jenkins_port
-            protocol = "tcp"
-            cidr_blocks = var.http_ingress_allowed_origin_cidrs
-            ipv6_cidr_blocks = null
-            prefix_list_ids = null
-            security_groups = null
-            self = null
-        },
-        {
-            description = "Allow SSH from Public"
-            from_port = 22
-            to_port = 22
-            protocol = "tcp"
-            cidr_blocks = var.http_ingress_allowed_origin_cidrs
-            ipv6_cidr_blocks = null
-            prefix_list_ids = null
-            security_groups = null
-            self = null
-        }
-
-    ]
-
-    egress = [
-        {
-            description = "Allow all egress"
-            prefix_list_ids = null
-            security_groups = null
-            self = null
-            from_port        = 0
-            to_port          = 0
-            protocol         = "-1"
-            cidr_blocks      = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = ["::/0"]
-        }
-    ]
-
     tags = var.common_tags
       
+}
+
+resource "aws_security_group_rule" "ssh-rule" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = var.http_ingress_allowed_origin_cidrs
+  ipv6_cidr_blocks  = null
+  security_group_id = aws_security_group.jenkins-server-sg.id
+}
+
+resource "aws_security_group_rule" "jenkins-rule" {
+  type              = "ingress"
+  from_port         = var.jenkins_port
+  to_port           = var.jenkins_port
+  protocol          = "tcp"
+  cidr_blocks       = var.http_ingress_allowed_origin_cidrs
+  ipv6_cidr_blocks  = null
+  security_group_id = aws_security_group.jenkins-server-sg.id
+}
+
+resource "aws_security_group_rule" "allow_all_egress" {
+    type              = "egress"
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.jenkins-server-sg.id
 }
